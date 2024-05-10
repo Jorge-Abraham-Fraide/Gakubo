226373860fc703962f2c5ed4aa439c97700e5762
Jorge-Abraham-Fraide <utm23090415@utma.edu.mx>
2024-05-10 13:19:26 -0600

    cambios a ConsultaAlumnos

--- src/TESTREACT_frontend/src/App.jsx
+++ src/TESTREACT_frontend/src/App.jsx
@@ -1,16 +1,35 @@
 // src/testreact_frontend/App.jsx
 
-import React from 'react';
+import React, { cloneElement } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import ConsultaAlumnos from './components/ConsultaAlumnos';
 import CargaAlumnos from './components/CargaAlumnos';
 import Inicio from './components/inicio';
-import './styles/commonStyles.css'; 
+import './styles/commonStyles.css';
 
 function Nav() {
   return (
-    <nav className="nav-bar">
-      <Link to="/" className="nav-link">Inicio</Link> 
+    <nav className="nav-bar" style={{ backgroundColor: 'rgba(0, 0, 0, 0)', marginTop: 500 }}>
+      <div style={{ fontSize: 20, display: 'flex', marginTop: -800 }}>
+        <Link to="/" className="nav-link"><img style={{ width: 100, height: 100, paddingRight: 100 }} src="./logo.png" /></Link>
+        <div style={{ display: 'flex' }}>
+          <div style={{ display: 'flex', backgroundColor: 'rgba(255, 255, 255, 0.2)' }}>
+            <div style={{ paddingLeft: 30 }}>
+              Inicio
+            </div>
+            <div style={{ paddingLeft: 40 }}>
+              Abejas Parasitadas
+            </div>
+            <div style={{ paddingLeft: 40, paddingRight: 30, }}>
+              Acerca de
+            </div>
+          </div>
+          <div style={{ paddingLeft: 100 }}>
+            Iniciar Sesión
+          </div>
+        </div>
+      </div>
+
     </nav>
   );
 }
--- src/TESTREACT_frontend/src/components/inicio.jsx
+++ src/TESTREACT_frontend/src/components/inicio.jsx
@@ -9,10 +9,9 @@ import '../styles/inicioStyles.css'; // Esta es la ruta relativa correcta desde 
 function Inicio() {
   return (
     <div className="main-content">
-      <img src="./PLANTEC.png" alt="Logo" className="logo" />
-      <div className="welcome-message">Bienvenidos a la Gestión de Alumnos</div>
-      <Link to="/consultar" className="nav-link"><button className="button">Consultar Alumnos</button></Link>
-      <Link to="/cargar" className="nav-link"><button className="button">Cargar Alumnos</button></Link>
+      <div className="welcome-message">Bienvenidos a Smart Honey</div>
+      <Link to="/consultar" className="nav-link"><button className="button">Consultar Datos</button></Link>
+      <Link to="/cargar" className="nav-link"><button className="button">Cargar Datos</button></Link>
     </div>
   );
 }
